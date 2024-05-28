23bb2dbae9208f10d37e527c79e290b7770c8f4b
Vasil <vasil.contact@gmail.com>
2024-05-28 19:01:49 +0200

    Dodani datoteki index.js in example.ts

---
+++ vasil_trajkovski/example.js
@@ -0,0 +1,5 @@
+console.log("To je example.ts datoteka");
+function sestevanje(a, b) {
+    return a + b;
+}
+console.log(sestevanje(2, 3));
---
+++ vasil_trajkovski/example.ts
@@ -0,0 +1,7 @@
+console.log("To je example.ts datoteka");
+
+function sestevanje(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(sestevanje(2, 3));
